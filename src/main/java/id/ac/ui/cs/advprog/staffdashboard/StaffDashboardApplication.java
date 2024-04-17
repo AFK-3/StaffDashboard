4baed6979ca2bc8ffd0ae0a977de9bb814a8ece9
RealPacifist <the.real.a.pacifist@gmail.com>
2024-04-17 17:11:21 +0700

    Initialization

---
+++ src/main/java/id/ac/ui/cs/advprog/staffdashboard/StaffDashboardApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.staffdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffDashboardApplication.class, args);
+	}
+
+}
